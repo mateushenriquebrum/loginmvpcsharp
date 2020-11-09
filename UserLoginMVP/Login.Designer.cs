006a7bf10b0a17b4bf5e90106f6bb00d3b166fab
Mateus Henrique Brum <mateushenriquebrum@gmail.com>
2020-11-09 21:14:20 +0000

    example complete

---
+++ UserLoginMVP/Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace UserLoginMVP
+{
+    partial class View
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layout = new System.Windows.Forms.TableLayoutPanel();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.userText = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.layout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.layout.ColumnCount = 1;
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layout.Controls.Add(this.userLabel, 0, 0);
+            this.layout.Controls.Add(this.userText, 0, 1);
+            this.layout.Controls.Add(this.passwordLabel, 0, 2);
+            this.layout.Controls.Add(this.passwordText, 0, 3);
+            this.layout.Controls.Add(this.loginButton, 0, 4);
+            this.layout.Location = new System.Drawing.Point(0, 0);
+            this.layout.Name = "layout";
+            this.layout.RowCount = 5;
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.layout.Size = new System.Drawing.Size(499, 187);
+            this.layout.TabIndex = 0;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(3, 0);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(38, 20);
+            this.userLabel.TabIndex = 0;
+            this.userLabel.Text = "User";
+            // 
+            // userText
+            // 
+            this.userText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userText.Location = new System.Drawing.Point(3, 40);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(493, 27);
+            this.userText.TabIndex = 1;
+            this.userText.TextChanged += new System.EventHandler(this.userText_TextChanged);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(3, 74);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(70, 20);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            // 
+            // passwordText
+            // 
+            this.passwordText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passwordText.Location = new System.Drawing.Point(3, 114);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.PasswordChar = '*';
+            this.passwordText.Size = new System.Drawing.Size(493, 27);
+            this.passwordText.TabIndex = 3;
+            this.passwordText.TextChanged += new System.EventHandler(this.passwordText_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoSize = true;
+            this.loginButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginButton.Enabled = false;
+            this.loginButton.Location = new System.Drawing.Point(3, 151);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(493, 33);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 187);
+            this.Controls.Add(this.layout);
+            this.Name = "View";
+            this.Text = "Login";
+            this.layout.ResumeLayout(false);
+            this.layout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel layout;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
